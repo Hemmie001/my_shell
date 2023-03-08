c2234d64d556ced5a529a4dbe20b3766bafa31b0
Sir-Blow <emmanuelakejelu@gmail.com>
2023-03-08 07:44:05 -0800

    Updated

--- main.c
+++ main.c
@@ -5,9 +5,13 @@ int main(int ac, char **argv)
 	char *prompt = "(my_shell) $ ";
 	char *lineptr;
 	size_t n = 0;
-	printf("%s", prompt);
-   	getline(&lineptr, &n, stdin);/*passes the variables to the getline function*/
-	free(lineptr);
-	return (0);
+	(void)ac;
+	(void)argv;/* declaring void variables */
 
+	printf("%s", prompt);
+
+	getline(&lineptr, &n, stdin);/*passes the variables to the getline function*/
+	printf("%s\n", lineptr);/*Enables us get what the user types in*/
+	free(lineptr);/*free memory, whether d function execution succeeds or fails*/
+	return (0);
 }
